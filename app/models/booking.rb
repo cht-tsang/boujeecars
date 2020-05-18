8ca8baddaea7ef06271e72c4fe48783b1674233e
chi tsang <cht.chihungtsang@gmail.com>
2020-05-18 17:26:58 +0100

    model validations added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+  validates :date, presence: true
+  validates :car, presence: true
+  validates :user, presence: true
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :price, presence: true
 end
