6eafab494793f4817ed274b83512807737867181
chi tsang <cht.chihungtsang@gmail.com>
2020-05-18 17:34:02 +0100

    syntax correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :cars, dependent: :destroy
   has_many :bookings, dependent: :destroy
 
-  validates email:, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
