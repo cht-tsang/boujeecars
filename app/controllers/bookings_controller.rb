b5474bbfc8016fa15adb09c14bae83b6aa859fbc
chi tsang <cht.chihungtsang@gmail.com>
2020-05-19 18:11:52 +0100

    bookings controller methods worked on

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,40 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @car = Car.find(params[:id])
+    @booking = Booking.new
+  end
+
+  def create
+    @car = Car.find(params[:id])
+    @booking = Booking.new()
+    @booking.car = @car
+    @booking.user = current_user
+    if @booking.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def booking_params
+  end
 end
