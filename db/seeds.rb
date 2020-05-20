bb7595061205ad7c6554d83280643d28ad952eb0
Anastasiia Makarova <erdbeer@yandex.ru>
2020-05-20 12:46:15 +0100

    view for booking index done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "date"
+
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+car = Car.new(make: 'NES', model: "A great console", price: 300, user: User.last)
+car.photo.attach(io: file, filename: 'car.png', content_type: 'image/png')
+car.save
+
+booking = Booking.new(start_date: Date.today, end_date: Date.today, user: User.last, car: car)
+booking.save
